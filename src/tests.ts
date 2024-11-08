8b8cec4959b478b73d554f346adb7fe815806207
Gabriel Kubota <gabrielkhiroshi@gmail.com>
2024-11-08 08:44:59 -0300

    fixing issues

--- src/tests.ts
+++ src/tests.ts
@@ -1,15 +1,16 @@
-import { getRelations, getWhere } from ".";
+import { getWhere } from ".";
+
+enum Teste {
+  ONE = "1",
+  TWO = "2",
+}
+
 console.log(
   getWhere([
     {
-      field: "debt.yourNumber",
-      operation: "ILIKE",
-      value: "%123%",
-    },
-    {
       field: "accountId",
       operation: "EQUAL",
-      value: "e3c6e38c-d987-4ac0-ab9d-de24e1e0cebd",
+      value: "one",
     },
   ])
 );
